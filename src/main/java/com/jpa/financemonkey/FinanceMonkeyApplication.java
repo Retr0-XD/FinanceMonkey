57f09e540a9425a2736ccbdcebc7074d7f6f16d9
Retr0-XD <88275564+Retr0-XD@users.noreply.github.com>
2025-06-01 13:27:04 +0530

    Inital Commit

---
+++ src/main/java/com/jpa/financemonkey/FinanceMonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.financemonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceMonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/financemonkey/FinanceMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.financemonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceMonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
